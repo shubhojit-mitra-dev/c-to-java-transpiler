19922056cd62567d58eb472499067b5e9a7b5b7a
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-01-20 18:48:05 +0530

    create: token types and header files

---
+++ TokenType.h
@@ -0,0 +1,46 @@
+#pragma once
+
+enum class TokenType {
+    // Keywords
+    INT,
+    FLOAT,
+    DOUBLE,
+    CHAR,
+    IF,
+    ELSE,
+    FOR,
+    WHILE,
+    RETURN,
+    PRINTF,
+
+    // Identifiers & literals
+    IDENTIFIER,
+    NUMBER,
+    STRING,
+
+    // Operators
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    MOD,
+
+    ASSIGN,        // =
+    EQ,            // ==
+    NEQ,           // !=
+    LT,            // <
+    GT,            // >
+    LTE,           // <=
+    GTE,           // >=
+
+    // Delimiters
+    LPAREN,        // (
+    RPAREN,        // )
+    LBRACE,        // {
+    RBRACE,        // }
+    SEMICOLON,     // ;
+    COMMA,
+
+    END_OF_FILE,
+    UNKNOWN
+};
